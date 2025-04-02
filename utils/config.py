5fb5be370eade6d4f727041e7f5a4056f63d914c
Suri-yi <yisiyeui_suri@outlook.com>
2025-04-02 15:12:54 +0800

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -2,10 +2,10 @@
 TIMEOUT = 90  # refresh page every 90s
 
 # =============== VISA CENTER SELECTION ===============
-CENTER = ('England', 'Manchester', 'Normal', 'Tourism')
+CENTER = ('China', 'Guangzhou', 'Normal', 'Tourism')
 # CENTER = ('Scotland', 'Edinburgh', 'Normal', 'Tourism')
 # CENTER = ('Bristol', 'London', 'Normal', 'Tourist')
 # =============== PERSONAL CONFIG FOR VISA ===============
-OPENED_PAGE = 'https://uk.blsspainvisa.com/visa4spain/book-date/QwertYU123'  # your BLS appointment page link
-EMAIL = '123@456.com'  # BLS email
-PASSWORD = 'BLSpassword'  # BLS password
+OPENED_PAGE = 'hhttps://spain.blscn.cn/CHN/Appointment/VisaType?data=N6S2S7MhTJClnSWV4M2uAj7R1%2BU658lE3AhISCjSuOoSujb%2BJhQ7mkrcbCiX9RZNeXKbQPXZ7F2BGzJYmlgq6z544r3kQjPHnWsyS7Xy0fVTbTmjDoOOpwIEnr4Xh0Js9gEqrO7qlvLVkujNYfFHQpziNqV8hMQJ9DYH3d3ottRwHfucfcQariWz3IIRTpc%2BoqXWrFcmTYp2cQfSulsns%2FzsGO%2FZyrh7%2BLfC3DPjeUU2G5l3hCfPvAiBQ0UChX%2F3M9vpTcz%2BLTi9rRzETH9XZBwsBfCYgxDLRNFEJdpEJxcC8x8of1QbpqfvzG8mS6GB'  # your BLS appointment page link
+EMAIL = 'yisiyeui_suri@outlook.com'  # BLS email
+PASSWORD = 'SiSi20031103'  # BLS password
